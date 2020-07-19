b3d90751fd166ed974fe56241e244c44a7a9dc1f
Andrew Aladjev <aladjev.andrew@gmail.com>
2020-07-19 20:54:37 +0300

    cosmetics

--- lib/common/format.rb
+++ lib/common/format.rb
@@ -4,11 +4,13 @@ FORMAT_PERCENT_ROUND_LENGTH = 2
 
 def format_percent(index, length)
   max_index = length - 1
-  return 100 if max_index.zero?
+  return "100.00" if max_index.zero?
 
-  (index.to_f * 100 / max_index)
-    .round(FORMAT_PERCENT_ROUND_LENGTH)
-    .to_s
+  percent = index.to_f * 100 / max_index
+  format(
+    "%.#{FORMAT_PERCENT_ROUND_LENGTH}f",
+    percent.round(FORMAT_PERCENT_ROUND_LENGTH)
+  )
 end
 
 def format_filesize(size)
