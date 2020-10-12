dab8e8cf5950c5e7b44d486a14fb017facc5a9a6
Andrew Aladjev <aladjev.andrew@gmail.com>
2020-10-13 02:17:32 +0300

    updated clear scripts

--- scripts/data/clear_results.sh
+++ scripts/data/clear_results.sh
@@ -6,6 +6,6 @@ cd "$DIR"
 
 cd "../.."
 
-echo "" | zstd -c > "data/valid_archives.zst"
+echo -n "" | zstd -c > "data/valid_archives.zst"
 cp "data/valid_archives.zst" "data/invalid_archives.zst"
 cp "data/valid_archives.zst" "data/volatile_archives.zst"
--- scripts/data/clear_urls.sh
+++ scripts/data/clear_urls.sh
@@ -6,6 +6,6 @@ cd "$DIR"
 
 cd "../.."
 
-echo "" | zstd -c > "data/valid_page_urls.zst"
+echo -n "" | zstd -c > "data/valid_page_urls.zst"
 cp "data/valid_page_urls.zst" > "data/invalid_page_urls.zst"
 cp "data/valid_page_urls.zst" > "data/archive_urls.zst"
