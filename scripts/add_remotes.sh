43faf9a09f010f9afb7a3045eaded177aeaf1811
Andrew Aladjev <aladjev.andrew@gmail.com>
2021-05-20 00:21:08 +0300

    cosmetics

--- scripts/add_remotes.sh
+++ scripts/add_remotes.sh
@@ -4,6 +4,6 @@ set -e
 DIR=$(dirname "${BASH_SOURCE[0]}")
 cd "$DIR"
 
-git remote add github    "git@github.com:andrew-aladev/lzws-archive-collector.git"    || true
-git remote add bitbucket "git@bitbucket.org:andrew-aladev/lzws-archive-collector.git" || true
-git remote add gitlab    "git@gitlab.com:andrew-aladev/lzws-archive-collector.git"    || true
+git remote add github    "git@github.com:andrew-aladev/lzws-archive-collector.git"    || :
+git remote add bitbucket "git@bitbucket.org:andrew-aladev/lzws-archive-collector.git" || :
+git remote add gitlab    "git@gitlab.com:andrew-aladev/lzws-archive-collector.git"    || :
